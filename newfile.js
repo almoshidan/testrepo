e7056d7ea1827f17f3a29a9675d61060f0bd460f
almoshidan <117280258+almoshidan@users.noreply.github.com>
2023-01-02 15:04:37 +0100

    add newfile

---
+++ newfile.js
@@ -0,0 +1,17 @@
+function average (numbers) {
+    let sum=0;
+    for (let number of numbers){
+        sum+=number;
+    }
+    return sum/numbers.length;
+}
+
+const averag=42
+
+let tomb = [1,2,3,4,5];
+let avg = average (tomb);
+console.log (avg);
+
+let average2=average;
+
+console.log (average2([1,2,3]))
